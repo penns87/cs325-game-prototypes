e10d2f94379df2b4b275d5b81038a5b401e17454
penns87 <70979785+penns87@users.noreply.github.com>
2021-02-10 18:42:36 -0500

    Digital Assignment 1

--- DA1/js/main.js
+++ DA1/js/main.js
@@ -15,25 +15,102 @@ class MyScene extends Phaser.Scene {
 
     constructor() {
         super();
-
+        this.platforms = null;
+        this.player = null;
+        this.cursors = null;
         this.bouncy = null;
+        this.button = null;
+        this.door = null;
     }
 
     preload() {
       this.load.image('sky', 'assets/sky.png');
       this.load.image('ground', 'assets/platform.png');
+      this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48 });
+      this.load.image('redbutton1', 'assets/RedButton1.png');
+      this.load.image('redbutton2', 'assets/RedButton3.png');
+      this.load.image('door1', 'assets/door1.png');
+      this.load.image('door2', 'assets/door2.png');
     }
 
     create() {
       this.add.image(400, 300, 'sky');
 
-      platforms = this.physics.add.staticGroup();
+      this.platforms = this.physics.add.staticGroup();
+      this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+      this.platforms.create(400, 400, 'ground');
 
-      platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+      this.player = this.physics.add.sprite(100, 450, 'dude');
+
+      this.player.setBounce(0.1);
+      this.player.setCollideWorldBounds(true);
+
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.button = this.physics.add.image(400, 375, 'redbutton1');
+    this.button.body.setAllowGravity(false);
+
+    this.door = this.physics.add.image(750, 500, 'door1').setScale(.25).refreshBody();
+    this.door.body.setAllowGravity(false);
+
+    this.physics.add.collider(this.player, this.platforms);
+
+    this.physics.add.overlap(this.player, this.button, (x,y) => this.pressButton(x,y));
+    }
+
+    pressButton(player, button) {
+      this.button.disableBody(true, true);
+      this.door.disableBody(true, true);
+      this.button = this.physics.add.image(400, 395, 'redbutton2');
+      this.button.body.setAllowGravity(false);
+      this.door = this.physics.add.image(625, 375, 'door2').setScale(.25).refreshBody();
+      this.door.body.setAllowGravity(false);
     }
 
     update() {
+      if (this.cursors.left.isDown)
+    {
+        this.player.setVelocityX(-160);
 
+        this.player.anims.play('left', true);
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.player.setVelocityX(160);
+
+        this.player.anims.play('right', true);
+    }
+    else
+    {
+        this.player.setVelocityX(0);
+
+        this.player.anims.play('turn');
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down)
+    {
+        this.player.setVelocityY(-330);
+    }
     }
 }
 
@@ -43,5 +120,5 @@ const game = new Phaser.Game({
     width: 800,
     height: 600,
     scene: MyScene,
-    physics: { default: 'arcade' },
+    physics: { default: 'arcade', arcade: { gravity: { y: 300 } } }
     });
